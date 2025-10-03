3d32c00f35742514d50444934e5f91548587e986
Hadi Ataya <hadiataya2005@gmail.com>
2025-10-04 00:38:22 +0100

    Completed third video changes

--- src/components/screens/ModuleListScreen.js
+++ src/components/screens/ModuleListScreen.js
@@ -1,20 +1,45 @@
-import { StyleSheet, Text } from 'react-native';
+import { Pressable, ScrollView, StyleSheet, Text, View } from 'react-native';
 import Screen from '../layout/Screen';
+
+import initalModules from '../../data/modules.js';
 
 const ModuleListScreen = () => {
   // Initialisation --------------
+  const modules = initalModules;
   // State ----------------
   // Handlers -------------
+  const handleSelect = () => alert ('Item selected')
   // View -------------
   return (
     <Screen>
-
-      <Text>List</Text>
-
+      <ScrollView style={styles.container}>
+      {modules.map((module) => {
+          return(
+          <Pressable key={module.ModuleCode} onPress={handleSelect}>
+            <View style={styles.item}>
+              <Text style={styles.text}>
+                {module.ModuleCode} {module.ModuleName}
+                </Text>
+            </View>
+            </Pressable>
+          )
+        })
+      }
+    </ScrollView>
     </Screen>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {},
+  item: {
+    paddingVertical: 15,
+    borderTopWidth: 1,
+    borderColor: 'lightgray',
+  },
+  text:{
+    fontSize: 16,
+  },
+});
 
 export default ModuleListScreen;
