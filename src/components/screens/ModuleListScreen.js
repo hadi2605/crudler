5cc61d7b03afe932375767c76c3b94e478d0fa3e
Hadi Ataya <hadiataya2005@gmail.com>
2025-10-09 13:04:22 +0100

    Completed changes

--- src/components/screens/ModuleListScreen.js
+++ src/components/screens/ModuleListScreen.js
@@ -1,5 +1,7 @@
-import { Pressable, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,} from 'react-native';
 import Screen from '../layout/Screen';
+import ModuleList from '../entity/modules/ModuleList.js';
+
 
 import initalModules from '../../data/modules.js';
 
@@ -8,38 +10,17 @@ const ModuleListScreen = () => {
   const modules = initalModules;
   // State ----------------
   // Handlers -------------
-  const handleSelect = () => alert ('Item selected')
+  const handleSelect = (module) => alert (`Item ${module.ModuleCode} selected`)
   // View -------------
   return (
     <Screen>
-      <ScrollView style={styles.container}>
-      {modules.map((module) => {
-          return(
-          <Pressable key={module.ModuleCode} onPress={handleSelect}>
-            <View style={styles.item}>
-              <Text style={styles.text}>
-                {module.ModuleCode} {module.ModuleName}
-                </Text>
-            </View>
-            </Pressable>
-          )
-        })
-      }
-    </ScrollView>
+      <ModuleList modules={modules} onSelect={handleSelect}/>
     </Screen>
   );
 }
 
 const styles = StyleSheet.create({
   container: {},
-  item: {
-    paddingVertical: 15,
-    borderTopWidth: 1,
-    borderColor: 'lightgray',
-  },
-  text:{
-    fontSize: 16,
-  },
 });
 
 export default ModuleListScreen;
