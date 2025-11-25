2e8493f8da877a95fb550b4a1bcae832c60636db
Hadi Ataya <164709198+hadi2605@users.noreply.github.com>
2025-11-25 23:48:41 +0000

    User selection feedback (#14)

--- src/components/entity/modules/ModuleItem.js
+++ src/components/entity/modules/ModuleItem.js
@@ -1,30 +1,35 @@
-import { Pressable, StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View } from 'react-native';
+import Selector from '../../UI/Selector';
 
-const ModuleItem = ({module, onSelect}) => {
-    // Intialisations --------------------------
-    // State -----------------------------------
-    // Handlers --------------------------------
-    //View -------------------------------------
-    return (   
-        <Pressable onPress={() => onSelect(module)}>
-        <View style={styles.item}>
-            <Text style={styles.text}>
-                {module.ModuleCode} {module.ModuleName}
-            </Text>
-        </View>
-        </Pressable>   
-    );
+const ModuleItem = ({ module, onSelect }) => {
+  // Intialisations --------------------------
+  // State -----------------------------------
+  // Handlers --------------------------------
+  const handleSelect = () => onSelect(module);
+  //View -------------------------------------
+  return (
+    <Selector onPress={handleSelect} pressedStyle={styles.pressedItem}>
+      <View style={styles.item}>
+        <Text style={styles.text}>
+          {module.ModuleCode} {module.ModuleName}
+        </Text>
+      </View>
+    </Selector>
+  );
 };
 
 const styles = StyleSheet.create({
-    item: {
-        paddingVertical: 15,
-        borderTopWidth: 1,
-        borderColor: 'lightgray',
-    },
-    text:{
-        fontSize: 16,
-    },
+  item: {
+    paddingVertical: 15,
+    borderTopWidth: 1,
+    borderColor: 'lightgray',
+  },
+  text: {
+    fontSize: 16,
+  },
+  pressedItem: {
+    backgroundColor: 'azure',
+  },
 });
 
 export default ModuleItem;
